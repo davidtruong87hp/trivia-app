9ed43f30900f450466820f5784b0971f4ab51718
davidtruong87hp <truongquangchien87@icloud.com>
2026-03-02 23:16:30 +0700

    added validation pipe and http exception filter

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`[NestJS] Running on http://localhost:${port}`);
 }
 bootstrap();
